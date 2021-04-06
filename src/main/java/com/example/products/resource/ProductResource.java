d88e43ed74739f886737c9d7f07b6f7adc17b48f
nsreddy0709 <64572455+nsreddy0709@users.noreply.github.com>
2021-04-06 19:34:39 +0530

    second commit

--- src/main/java/com/example/products/resource/ProductResource.java
+++ src/main/java/com/example/products/resource/ProductResource.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/rest/products")
+@RequestMapping(value = "/v1")
 
 
 public class ProductResource {
